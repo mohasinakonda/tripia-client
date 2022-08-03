76382f28d98b06c9d033366fe8fd49ce32da651d
mohainakonda <mohasinakonda39@gmail.com>
2022-08-03 22:12:26 +0600

    partener page added

--- src/Pages/PartnerPrograms/Partner.js
+++ src/Pages/PartnerPrograms/Partner.js
@@ -1,23 +1,23 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Partner = () => {
   return (
-    <div>
-      <div className="relative border px-[60px] py-[40px]  rounded-full  lg:h-96 lg:w-[845px] sm:h-[845px] sm:w-96  ">
-        <div className="border-dotted pt-12">
-          <div className="grid lg:grid-cols-3 items-center justify-center ">
-            <div className="relative bg-green-500 text-center mt-8 p-5">1</div>
-            <div className="relative  text-center">2</div>
-            <div className="relative  text-center mt-8">3</div>
-          </div>
-
-          <div className="relative">4</div>
-          <div className="grid lg:grid-cols-3 ">
-            <div className="relative">5</div>
-            <div className="relative">6</div>
-            <div className="relative ">7</div>
-          </div>
-        </div>
+    <div className="flex">
+      <h2 className="text-2xl w-96">Register for </h2>
+      <nav className="flex gap-2 text-xl">
+        <Link to="invoices" className="border p-3 shadow-xl rounded-md">
+          Listing Hotel
+        </Link>
+        <Link to="dashboard" className="border p-3 shadow-xl rounded-md">
+          Listing bus
+        </Link>
+        <Link to="dashboard" className="border p-3 shadow-xl rounded-md">
+          Become guide
+        </Link>
+      </nav>
+      <div className="content">
+        <Outlet />
       </div>
     </div>
   );
